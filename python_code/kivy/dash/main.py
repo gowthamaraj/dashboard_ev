036c8d2b0c9551382552ef74b093339d1ab4fe59
gowthamaraj <gowthamaraj360@gmail.com>
2019-03-27 22:03:00 +0530

    basic version done dash 1.0

---
+++ python_code/kivy/dash/main.py
@@ -0,0 +1,49 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.progressbar import ProgressBar
+from kivy.clock import Clock
+from random import random
+
+
+class RootWidget(GridLayout):
+    pass
+
+class RevElectricApp(App):
+    def build(self):
+        return RootWidget()
+    
+    def on_start(self):
+        Clock.schedule_interval(self.update, 0.3)
+
+    def update(self, nap):
+        x=int(random()*100)
+        self.root.ids.speed.text = str( str(x) +' m/s')
+        
+        t1="[b][color=f47141]Temperature [/color][/b][color=f47141] \n [size=40] %d [/size] [/color]" % (x)
+        self.root.ids.t1.text = t1
+        
+        t2="[b][color=f47141]Temperature [/color][/b][color=f47141] \n [size=40] %d [/size] [/color]" % (x)
+        self.root.ids.t2.text = t2
+        
+        t3="[b][color=f47141]Temperature [/color][/b][color=f47141] \n [size=40] %d [/size] [/color]" % (x)
+        self.root.ids.t3.text = t3
+        
+        t4="[b][color=f47141]Temperature [/color][/b][color=f47141] \n [size=40] %d [/size] [/color]" % (x)
+        self.root.ids.t4.text = t4
+        
+        soc=int(random()*100)
+        self.root.ids.soc.value =soc
+        self.root.ids.soc_value.text =str(soc)
+
+
+
+if __name__ == "__main__":
+    RevElectricApp().run()
+    
+    
+    
+    
+    
+    
+    
+    
